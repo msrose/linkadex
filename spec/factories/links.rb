20494dd157188aa5cc3649bf39bc918df4d95a74
Michael Rose <ekimsc1094@sympatico.ca>
2013-06-30 02:39:14 -0400

    Add factories for all models.

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :link do
+  factory :link do |f|
+    f.tile_id { FactoryGirl.create(:tile).id }
+    f.href { Faker::Internet.url }
+    f.title { Faker::Lorem.words(1).first.capitalize }
+    # f.color { random_hex }
+    f.target { %w(blank self).sample }
   end
 end
--- spec/factories/tiles.rb
+++ spec/factories/tiles.rb
@@ -1,6 +1,13 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+def random_hex
+  '#' + ['A'..'F', '0'..'9'].map(&:to_a).flatten.sample(6).join
+end
+
 FactoryGirl.define do
-  factory :tile do
+  factory :tile do |f|
+    f.group_id { FactoryGirl.create(:group).id }
+    f.border_color { random_hex }
+    f.background_color { random_hex }
   end
 end
