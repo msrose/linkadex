40d0b809ce843562faeac2c40af05f43d0a728ee
Michael Rose <ekimsc1094@sympatico.ca>
2013-06-30 02:41:53 -0400

    Make all tests for group model pass.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ActiveRecord::Base
-  # attr_accessible :title, :body
+  has_many :tiles
+  has_many :links, :through => :tiles
+
+  validates :title, :presence => true
+
+  VALID_COLOR_REGEX = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
+  validates :color, :presence => true, :format => { :with => VALID_COLOR_REGEX }
 end
