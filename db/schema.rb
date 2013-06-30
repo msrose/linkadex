8ea1495f902fa72835810f08510cb7eaccb3e53a
Michael Rose <ekimsc1094@sympatico.ca>
2013-06-30 04:39:21 -0400

    Migrate database to change color columns to foreign keys.

--- db/schema.rb
+++ db/schema.rb
@@ -11,34 +11,34 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630045702) do
+ActiveRecord::Schema.define(:version => 20130630080030) do
 
   create_table "groups", :force => true do |t|
     t.string   "title"
-    t.string   "color"
+    t.integer  "color_id",   :limit => 255
     t.boolean  "collapsed"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   create_table "links", :force => true do |t|
     t.integer  "tile_id"
     t.string   "title"
     t.string   "href"
-    t.string   "color"
+    t.integer  "color_id",   :limit => 255
     t.string   "target"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
   end
 
   add_index "links", ["tile_id"], :name => "index_links_on_tile_id", :unique => true
 
   create_table "tiles", :force => true do |t|
     t.integer  "group_id"
-    t.string   "border_color"
-    t.string   "background_color"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.integer  "border_color_id",     :limit => 255
+    t.integer  "background_color_id", :limit => 255
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
   end
 
   add_index "tiles", ["group_id"], :name => "index_tiles_on_group_id"
