5b27a0d94fda1e2b8a201a1341967f6f12dd90c6
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-01 18:18:51 -0400

    Account for empty string color alias when populating dropdown in groups controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,6 +38,6 @@ class GroupsController < ApplicationController
     end
 
     def load_colors
-      @colors = Color.all.collect { |color| [color.alias || color.hex_value, color.id] }
+      @colors = Color.all.collect { |color| [color.alias.blank? ? color.hex_value : color.alias, color.id] }
     end
 end
