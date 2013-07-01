41af618107976c5377cfab212c8afe3a8a87a411
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-01 17:51:54 -0400

    Make all uniqueness tests pass with validations in models.

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,6 @@ class Link < ActiveRecord::Base
   belongs_to :tile
   belongs_to :color
 
-  validates :tile_id, :href, :title, :target, :color_id, :presence => true
+  validates :tile_id, :target, :color_id, :presence => true
+  validates :title, :href, :presence => true, :uniqueness => true
 end
