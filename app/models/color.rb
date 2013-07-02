1b92006a6660e29b27fd2d3da555a5ac34db056d
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-01 20:43:35 -0400

    Add before save in color model to upcase hex value. Closes #15.

--- app/models/color.rb
+++ app/models/color.rb
@@ -2,6 +2,7 @@ class Color < ActiveRecord::Base
   has_many :groups
   has_many :tiles
   has_many :links
+  before_save { |color| color.hex_value.upcase! }
 
   VALID_COLOR_REGEX = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
   validates :hex_value, :format => { :with => VALID_COLOR_REGEX }, :uniqueness => true
