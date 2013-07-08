6247dedc62163a46a7a871b8f53e90fa76631bdb
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-08 16:42:22 -0400

    Provide basic styling for links index page.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+def random_hex
+  '#' + ['A'..'F', '0'..'9'].map(&:to_a).flatten.sample(6).join
+end
+
 Color.destroy_all
 Group.destroy_all
+Link.destroy_all
 
 10.times do
-  Color.create(:hex_value => '#' + ['A'..'F', '0'..'9'].map(&:to_a).flatten.sample(6).join,
+  Color.create(:hex_value => random_hex,
                :alias => [nil, Faker::Lorem.words(1).first.capitalize].sample)
 end
 
@@ -18,4 +23,15 @@ end
   Group.create(:title => Faker::Lorem.words(1).first.capitalize,
                :color_id => Color.all.sample.id,
                :collapsed => [true, false].sample)
+end
+
+Group.all.each do |group|
+  (rand(5) + 5).times do
+    group.links.create(:title => Faker::Lorem.words(1).first.capitalize,
+                      :href => Faker::Internet.url,
+                      :color_id => Color.all.sample.id,
+                      :background_color_id => Color.all.sample.id,
+                      :border_color_id => Color.all.sample.id,
+                      :target => %w(self blank).sample)
+  end
 end
