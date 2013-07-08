2856c7c2a49ac57c68814e9c37ac7287283ddea2
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-08 19:35:57 -0400

    Validate link href with URI.regexp.

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,5 +7,6 @@ class Link < ActiveRecord::Base
   belongs_to :background_color, :class_name => 'Color', :foreign_key => :background_color_id
 
   validates :target, :group_id, :color_id, :border_color_id, :background_color_id, :presence => true
-  validates :title, :href, :presence => true, :uniqueness => true
+  validates :title, :presence => true, :uniqueness => true
+  validates :href, :format => { :with => URI.regexp }
 end
