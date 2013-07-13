3885a6accc207b685c45ab47b94b70fab027a88d
Michael Rose <ekimsc1094@sympatico.ca>
2013-07-13 00:40:22 -0400

    Change migration for compatibility with production database.

--- db/migrate/20130630080030_change_colors_to_foreign_keys.rb
+++ db/migrate/20130630080030_change_colors_to_foreign_keys.rb
@@ -1,23 +1,23 @@
 class ChangeColorsToForeignKeys < ActiveRecord::Migration
   def up
-    change_column :groups, :color, :integer
-    rename_column :groups, :color, :color_id
-    change_column :tiles, :border_color, :integer
-    rename_column :tiles, :border_color, :border_color_id
-    change_column :tiles, :background_color, :integer
-    rename_column :tiles, :background_color, :background_color_id
-    change_column :links, :color, :integer
-    rename_column :links, :color, :color_id
+    add_column :groups, :color_id, :integer
+    remove_column :groups, :color
+    add_column :tiles, :border_color_id, :integer
+    remove_column :tiles, :border_color
+    add_column :tiles, :background_color_id, :integer
+    remove_column :tiles, :background_color
+    add_column :links, :color_id, :integer
+    remove_column :links, :color
   end
 
   def down
-    change_column :groups, :color_id, :string
-    rename_column :groups, :color_id, :color
-    change_column :tiles, :border_color_id, :string
-    rename_column :tiles, :border_color_id, :border_color
-    change_column :tiles, :background_color_id, :string
-    rename_column :tiles, :background_color_id, :background_color
-    change_column :links, :color_id, :string
-    rename_column :links, :color_id, :color
+    add_column :groups, :color, :string
+    remove_column :groups, :color_id
+    add_column :tiles, :border_color, :string
+    remove_column :tiles, :border_color_id
+    add_column :tiles, :background_color, :string
+    remove_column :tiles, :background_color_id
+    add_column :links, :color, :string
+    remove_column :links, :color_id
   end
 end
