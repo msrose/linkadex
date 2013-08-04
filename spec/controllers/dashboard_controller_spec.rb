732916f07875f5583eaa1b29709088568d47e199
Michael Rose <ekimsc1094@sympatico.ca>
2013-08-04 14:07:07 -0400

    Add dashboard controller tests.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,4 +1,16 @@
 require 'spec_helper'
 
 describe DashboardController do
+  describe "GET #home" do
+    it "renders the home template" do
+      get :home
+      response.should render_template :home
+    end
+
+    it "populates an array of groups" do
+      group = FactoryGirl.create(:group)
+      get :home
+      assigns(:groups).should == [group]
+    end
+  end
 end
