5b86b402ca76b22f84f7fc7389631d39fc1e6852
Michael Rose <ekimsc1094@sympatico.ca>
2014-09-16 22:33:16 -0400

    Fix problem with require order in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-colorpicker.js
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
 
 $(document).ready(function() {
